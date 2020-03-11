e13dbb179a09988e2dfb46e11fba7c30cf690600
ReubenJCarter <reubenjcarter@gmail.com>
2020-03-11 16:36:36 +0100

    Added to the java test

--- test/JavaTest.java
+++ test/JavaTest.java
@@ -1,7 +1,11 @@
+//run like   java -cp .\* JavaTest aruco.png
+
 import VisiJava.Visi.Color;
 import VisiJava.Visi.*;
 import VisiJava.Visi.IO.*;
-//run like   java -cp .\* JavaTest 
+import VisiJava.Visi.Process.*;
+import VisiJava.Visi.CompositeProcess.*;
+
 public class JavaTest {
 
     static {
@@ -11,18 +15,52 @@ public class JavaTest {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         
+        Context context = new Context(); 
+	    context.MakeCurrent(); 
+
+        System.out.println("creating color" );
         Color c = new Color(); 
         c.setR(1.0f); 
         System.out.println("Color:c.R=" + Float.toString(c.getR()));
 
+        Image image1= new Image(); 
+		Image image2 = new Image();
+		Image image3= new Image();
 
+		ImageGPU imageGPU1 = new ImageGPU();
+		ImageGPU imageGPU2 = new ImageGPU();
+		ImageGPU imageGPU3 = new ImageGPU();
+		ImageGPU imageGPU4 = new ImageGPU();
 
         System.out.println("args[0]" + args[0]);
-        Image image0 = new Image();
         
-        ImageFile.Read(args[0], image0); 
+        ImageFile.Read(args[0], image1); 
 
-        ImageFile.Write("JavaTest1.png", image0); 
+        ImageFile.Write("JAVATest1.png", image1); 
+
+        imageGPU1.Copy(image1); 
+
+
+        //Brightness Contrast
+		System.out.println( "Brightness Contrast\n");
+		BrightnessContrast brightnessContrast = new BrightnessContrast(); 
+		brightnessContrast.SetBrightness(-1); 
+		brightnessContrast.SetContrast(3); 
+		brightnessContrast.Run(imageGPU1, imageGPU2);
+		image2.Copy(imageGPU2);
+		ImageFile.Write("JAVAimage1_1Test.png", image2);
+		brightnessContrast.Run(image1, image2); 
+		ImageFile.Write("JAVAimage1_2Test.png", image2);
+
+        //Canny Edges
+		System.out.println("Canny Edges\n");
+		CannyEdgeDetect canny = new CannyEdgeDetect(); 
+		canny.SetBlurSigma(0.5f); 
+		canny.SetLowEdgeThreshold(0.1f);
+		canny.SetHighEdgeThreshold(0.2f);
+
+		canny.Run(imageGPU1, image2); 
+		ImageFile.Write("JAVAimage12_1Test.png", image2);
     }
 
 }
