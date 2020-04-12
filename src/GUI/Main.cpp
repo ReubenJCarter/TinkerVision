798dd40bdd28ba6a377dbcb2a9f77b5947dc2dff
ReubenJCarter <reubenjcarter@gmail.com>
2020-04-12 11:25:27 +0200

    Added QT Dark mode.

--- src/GUI/Main.cpp
+++ src/GUI/Main.cpp
@@ -1,11 +1,33 @@
-#include <QtWidgets/QApplication>
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+
+#include <iostream> 
 
 #include "MainWindow.h"
 
 int main(int argc, char* argv[])
 {
+	//create app
 	QApplication app(argc, argv);
 	
+	//Set Style sheet
+	QFile f(":qdarkstyle/style.qss");
+
+	if (!f.exists())   
+	{
+		std::cout << "Unable to set stylesheet, file not found\n";
+	}
+	else   
+	{
+		f.open(QFile::ReadOnly | QFile::Text);
+		QTextStream ts(&f);
+		qApp->setStyleSheet(ts.readAll());
+	}
+
+	//create main window
 	Visi::GUI::MainWindow mainWindow;
+	
+	
     return app.exec();
 }
